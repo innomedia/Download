999d512484c80ddd7a97f6275082d631e8453bcd
chromos33 <chromos33@users.noreply.github.com>
2024-05-13 12:09:22 +0200

    Update DownloadModuleController.php

--- src/Controllers/DownloadModuleController.php
+++ src/Controllers/DownloadModuleController.php
@@ -5,4 +5,10 @@ use PageController;
 
 class DownloadModuleController extends PageController
 {
+  protected function init()
+  {
+      parent::init();
+      $this->extend('updateDownloadModuleInit');
+  }
+  
 }
